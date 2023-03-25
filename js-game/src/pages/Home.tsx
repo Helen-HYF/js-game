e984a76f332272cb71020ad75319ce302330e29c
HelenT HYF <107499395+HelenT-HYF@users.noreply.github.com>
2023-03-25 16:51:06 +0100

    added pages and modified the default setup of ionic

---
+++ js-game/src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonPage,
+} from "@ionic/react";
+import React from "react";
+import "./Home.css";
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen color={"warning"}>
+        <div className="container">
+          <IonCard color={"success"}>
+            <IonCardContent>
+              <h1>Js Conditionals</h1>
+              <br />
+              <h1>Quiz</h1>
+            </IonCardContent>
+          </IonCard>
+          <IonCard color={"success"}>
+            <IonCardContent>
+              <IonButton color={"success"} routerLink="/Mainpage">
+                <h1>Start</h1>
+              </IonButton>
+            </IonCardContent>
+          </IonCard>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
