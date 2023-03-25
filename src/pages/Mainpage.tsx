bfc68469813bab7d5aae576752e5b0ae1456d18b
HelenT HYF <107499395+HelenT-HYF@users.noreply.github.com>
2023-03-25 18:09:25 +0100

    adding pages and modified css classes

---
+++ src/pages/Mainpage.tsx
@@ -0,0 +1,24 @@
+import {
+    IonPage,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonButton,
+  } from "@ionic/react";
+import React from "react";
+  import Quiz from "../components/quiz";
+  
+  const Mainpage: React.FC = () => {
+    const intscore = 0;
+    return (
+      <IonPage>
+        <IonContent fullscreen>
+        
+          <Quiz />
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Mainpage;
+  
